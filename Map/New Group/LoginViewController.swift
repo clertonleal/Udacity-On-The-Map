f8e17b04c1e1d9f6e02180342298aa41c77fb93a
Clerton Leal <clerton.leal@bancointer.com.br>
2020-04-26 15:42:49 -0300

    initial commit

---
+++ Map/New Group/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  Map
+//
+//  Created by Clêrton Cunha Leal on 26/04/20.
+//  Copyright © 2020 Clêrton Cunha Leal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    private let signupUrl = "https://auth.udacity.com/sign-up"
+    
+    @IBOutlet weak var textEmail: UITextField!
+    @IBOutlet weak var textPassword: UITextField!
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    @IBAction func onLogin(_ sender: Any) {
+        print("Email: \(String(describing: textEmail.text))")
+        print("Password: \(String(describing: textPassword.text))")
+        performSegue(withIdentifier: "goToMap", sender: nil)
+    }
+}
